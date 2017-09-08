3061e608dc1c1fa663c8b0bd7a17f0507bcde423
Nikunj Jain <nikunj.jain@berkeley.edu>
2017-09-08 02:17:14 +0000

    Finished part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -11,11 +11,14 @@ class HangpersonGame
   attr_accessor :word
   attr_accessor :guesses
   attr_accessor :wrong_guesses
+  attr_accessor :word_with_guesses
+  attr_accessor :check_win_or_lose
   
   def initialize(word)
     @word = word
     @guesses = ''
     @wrong_guesses = ''
+    @word_with_guesses = word.gsub(/[a-zA-Z]/, "-")
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -31,15 +34,20 @@ class HangpersonGame
   end
   
   def guess(character)
-
+    raise ArgumentError.new unless !character.nil? and  character != '' and character =~ /^[a-zA-Z]+$/
     if @word =~ /#{character}/i
       if @guesses !~ /#{character}/i
         @guesses += character
+        (0 ... @word.length).find_all { |i| @word[i,1] == character }.each do |i|
+          @word_with_guesses[i] = character
+        end
+        @check_win_or_lose = ( @word_with_guesses.include?("-") ? :play : :win)
         return true
       end
     else
       if @wrong_guesses !~ /#{character}/i
         @wrong_guesses += character
+        @check_win_or_lose = ( @wrong_guesses.length.eql?(7) ? :lose : :play)
         return true
       end
     end
